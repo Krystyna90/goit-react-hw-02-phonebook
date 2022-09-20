5a458f9462618583735fe646d2890bea48a5b4bc
ukrbride4u <ukrbride4u@gmail.com>
2022-09-20 15:12:08 +0300

    changes

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,41 +1,39 @@
 import React, { Component } from "react";
-import PropTypes from 'prop-types';
-import css from './ContactForm.module.css';
-
+import PropTypes from "prop-types";
+import css from "./ContactForm.module.css";
 
 class ContactForm extends Component {
-    
-    state = {
-        name: '',
-        number:''
-    };
+  state = {
+    name: "",
+    number: "",
+  };
 
-    handleInputChange = e => { 
-        this.setState({
-            [e.currentTarget.name]: e.currentTarget.value,
-        })
-    }
+  handleInputChange = (e) => {
+    this.setState({
+      [e.currentTarget.name]: e.currentTarget.value,
+    });
+  };
 
-    handleSubmit = e => {
-        e.preventDefault();
-        this.props.onSubmit(this.state);
-        this.reset();
-    };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.onSubmit(this.state);
+    this.reset();
+  };
 
-    reset = () => {
-        this.setState({
-             name: '',
-        })
-    }
+  reset = () => {
+    this.setState({
+      name: "",
+      number: "",
+    });
+  };
 
-    render() {
-        
-   return(
-   <form onSubmit ={this.handleSubmit} className={css.Contact_form}>
-           <label className={css.Contact_label}>
-               Name
-               <input
-                   className={css.Contact_input}
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className={css.Contact_form}>
+        <label className={css.Contact_label}>
+          Name
+          <input
+            className={css.Contact_input}
             type="text"
             name="name"
             value={this.state.name}
@@ -43,32 +41,32 @@ class ContactForm extends Component {
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
-               />
-           </label>
+          />
+        </label>
 
-           <label className={css.Contact_label}>
-               Number 
-               <input
-                   className={css.Contact_input}
-  type="tel"
-                   name="number"
-                   value={this.state.number}
-                   onChange={this.handleInputChange}
-  pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-  title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-  required
-/>
-           </label>
-        <button className={css.Contact_btn} type="submit">Add to contact</button>
-            </form>
-   )
-         }
-};
+        <label className={css.Contact_label}>
+          Number
+          <input
+            className={css.Contact_input}
+            type="tel"
+            name="number"
+            value={this.state.number}
+            onChange={this.handleInputChange}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </label>
+        <button className={css.Contact_btn} type="submit">
+          Add to contact
+        </button>
+      </form>
+    );
+  }
+}
 
 ContactForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
-  name: PropTypes.string.isRequired,
-  number: PropTypes.string.isRequired,
 };
 
 export default ContactForm;
--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,18 +1,17 @@
 import React from "react";
-import PropTypes from 'prop-types';
-import css from './Filter.module.css';
+import PropTypes from "prop-types";
+import css from "./Filter.module.css";
 
 const Filter = ({ value, onChange }) => (
-       <label className={css.Filter_label}>
-           Find contact by name
+  <label className={css.Filter_label}>
+    Find contact by name
     <input
       className={css.Filter_input}
-            type="text"
-               value={value}
-               onChange={onChange}
-               />
-        </label>
-         
+      type="text"
+      value={value}
+      onChange={onChange}
+    />
+  </label>
 );
 
 Filter.propTypes = {
